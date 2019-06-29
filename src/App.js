1763200cd35a669d8f044b5010aa6b33332b9750
dev.koold <dev.koold@gmail.com>
2019-06-29 22:12:53 +0530

    landing page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,136 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import ReactCountryFlag from "react-country-flag";
+import axios from 'axios';
+import './assets/vendor/font-awesome/css/font-awesome.css';
+import './assets/vendor/nucleo/css/nucleo.css';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor(){
+    super();
+    
+  }
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="header-global">
+          <nav id="navbar-main" className="navbar navbar-main navbar-expand-lg navbar-transparent navbar-light">
+            <div className="container">
+              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar_global" aria-controls="navbar_global" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="navbar-collapse collapse" id="navbar_global">
+                <div className="navbar-collapse-header">
+                  <div className="row">
+
+                    <div className="col-6 collapse-close">
+                      <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navbar_global" aria-controls="navbar_global" aria-expanded="false" aria-label="Toggle navigation">
+                        <span></span>
+                        <span></span>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <ul className="navbar-nav navbar-nav-hover align-items-lg-center">
+                  <li className="nav-item dropdown">
+                    <a href="#" className="nav-link" data-toggle="dropdown" href="#" role="button">
+                      <i className="ni ni-ui-04 d-lg-none"></i>
+                      <span className="nav-link-inner--text">My Loans</span>
+                    </a>
+                  </li>
+                  <li className="nav-item dropdown">
+                    <a href="#" className="nav-link" data-toggle="dropdown" href="#" role="button">
+                      <i className="ni ni-collection d-lg-none"></i>
+                      <span className="nav-link-inner--text">Create Loan Request</span>
+                    </a>
+                  </li>
+                  <li className="nav-item dropdown">
+                    <a href="#" className="nav-link" data-toggle="dropdown" href="#" role="button">
+                      <i className="ni ni-ui-04 d-lg-none"></i>
+                      <span className="nav-link-inner--text">Create Loan Offers</span>
+                    </a>
+                  </li>
+                  <li className="nav-item dropdown">
+                    <a href="#" className="nav-link" data-toggle="dropdown" href="#" role="button">
+                      <i className="ni ni-collection d-lg-none"></i>
+                      <span className="nav-link-inner--text">View All Request</span>
+                    </a>
+                  </li>
+                  <li className="nav-item dropdown">
+                    <a href="#" className="nav-link" data-toggle="dropdown" href="#" role="button">
+                      <i className="ni ni-ui-04 d-lg-none"></i>
+                      <span className="nav-link-inner--text">View All Offers</span>
+                    </a>
+                  </li>
+
+                  <li className="nav-item dropdown">
+                    <a href="#" className="nav-link" data-toggle="tooltip" title="Statistics" href="#" role="button">
+                    <i class="fa fa-bar-chart" aria-hidden="true"></i>
+                    </a>
+                  </li>
+
+                  <li className="nav-item dropdown">
+                    <a href="#" className="nav-link" data-toggle="dropdown" href="#" role="button">
+                      <span className="nav-link-inner--text" style={{color:'white'}}> <i className="fa fa-question-circle" aria-hidden="true"></i></span>
+
+                    </a>
+                    <div className="dropdown-menu">
+                      <a href="#" className="dropdown-item">FAQ</a>
+                      <a href="#" className="dropdown-item">Legal</a>
+                      <a href="#" className="dropdown-item">Fees</a>
+                      <a href="#" className="dropdown-item">Live Support</a>
+                      <a href="#" className="dropdown-item">Submit Ticket</a>
+                    </div>
+                  </li>
+
+                  <li className="nav-item dropdown">
+                  <ReactCountryFlag code="IN" svg />
+                  </li>
+
+
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </header>
+          <div className="position-relative">
+            <section className="section-hero section-shaped my-0">
+              <div className="shape shape-style-1 shape-primary">
+                <span className="span-150"></span>
+                <span className="span-50"></span>
+                <span className="span-50"></span>
+                <span className="span-75"></span>
+                <span className="span-100"></span>
+                <span className="span-75"></span>
+                <span className="span-50"></span>
+                <span className="span-100"></span>
+                <span className="span-50"></span>
+                <span className="span-100"></span>
+              </div>
+              <div className="container shape-container d-flex align-items-center">
+                <div className="col px-0">
+                  <div className="row justify-content-center align-items-center">
+                    <div className="col-lg-7 text-center">
+                      <h1 style={{color:'white'}}> THE GLOBAL LENDING MARKETPLACE</h1>
+                      <div className="btn-wrapper" style={{marginTop:'80px'}}>
+                        <a href="#" className="btn btn-info btn-icon mb-3 mb-sm-0" data-toggle="scroll">
+                          <span className="btn-inner--icon"><i className="fa fa-calculator"></i></span>
+                          <span className="btn-inner--text">Loan Calculator</span>
+                        </a>
+                        <a href="#" className="btn btn-white btn-icon mb-3 mb-sm-0">
+                          <span className="btn-inner--icon"><i className="ni ni-money-coins"></i></span>
+                          <span className="btn-inner--text">Start Borrowing</span>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+          </section>
+        </div>
     </div>
   );
+}
 }
 
 export default App;
