ade905e05a6245bc844f66c070c00e66f9f29bf4
devilla <dev.koold@gmail.com>
2019-09-21 18:15:36 +0530

    waiting for collateral & fund now on view all requests

--- src/components/pages/ViewAllRequests.js
+++ src/components/pages/ViewAllRequests.js
@@ -309,7 +309,7 @@ class ViewAllRequests extends Component {
             <div className="ml-4 row">
               {
                 this.state.loanAmount.map((loanAmount,i)=>{
-                return <div className="col-sm-4">
+                return status[i]>0 && <div className="col-sm-4">
                  <div className="card">
                    <div className="card-header">
 
