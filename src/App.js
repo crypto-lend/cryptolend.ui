c422e00d9a7bc00711c405a5e16b44b869f8d874
Devilla <dev.koold@gmail.com>
2019-09-27 00:10:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React  from 'react';
 import './assets/vendor/font-awesome/css/font-awesome.css';
 import './assets/vendor/nucleo/css/nucleo.css';
-import './assets/css/argon.min.css';
+// import './assets/css/argon.min.css';
 import './App.css';
 
 import { Provider } from 'react-redux';
