b674278b387d183a86f1ac65986476d4967f41c9
jashanbhullar <jsbhullar96@gmail.com>
2019-09-29 23:32:46 +0530

    - Removed junk code - Added macbook screen

--- src/components/pages/LandingPage.js
+++ src/components/pages/LandingPage.js
@@ -1,17 +1,11 @@
 import React, { Component } from "react";
-import ReactCountryFlag from "react-country-flag";
-import { InstallMetaMask } from "../Web3/InstallMetaMask";
-import { EthereumIcon } from "../Web3/EthereumIcon";
-import {
-  fetchNetwork,
-  fetchAccounts,
-  getAccounts
-} from "../../services/Web3Service";
-import blocklendr from "../../assets/img/brand/blocklendrLogo.jpg";
+
+import { fetchAccounts } from "../../services/Web3Service";
 import P2pPoints from "./landingpagecomponents/p2pworks/p2pPoints";
 import Blocklendr from "./landingpagecomponents//blockLenderWorks/blockleander";
 import Clients from "./landingpagecomponents/ourclients/clients";
 import Navbar from "./Navbar";
+import macbookScreen from "../images/macbook.png";
 
 class HomePage extends Component {
   constructor(props) {
@@ -42,26 +36,12 @@ class HomePage extends Component {
   };
 
   render() {
-    const web3 = window.web3;
     return (
       <div>
         <div className="HomePage text-center" style={{ color: "fff" }}>
           <Navbar />
-          <div className="position-relative">
+          <div className="position-relative pt-5">
             <section className="section-hero section-shaped my-0">
-              <div className="shape shape-style-1 shape-primary">
-                <span className="span-150"></span>
-                <span className="span-50"></span>
-                <span className="span-50"></span>
-                <span className="span-75"></span>
-                <span className="span-100"></span>
-                <span className="span-75"></span>
-                <span className="span-50"></span>
-                <span className="span-100"></span>
-                <span className="span-50"></span>
-                <span className="span-100"></span>
-              </div>
-
               <div className="container shape-container d-flex align-items-center">
                 <div className="col px-0">
                   <div className="pricing card-group flex-column flex-md-row mb-3">
@@ -81,72 +61,17 @@ class HomePage extends Component {
                       </div>
 
                       <div className="card-body" style={{ margin: "10%" }}>
-                        <ul className="list-unstyled my-4">
-                          <li>
-                            <div className="d-flex align-items-center">
-                              <div>
-                                <div className="icon icon-xs"></div>
-                              </div>
-                              <div></div>
-                            </div>
-                          </li>
-                          <li>
-                            <div className="d-flex align-items-center">
-                              <div>
-                                <div className="icon icon-xs "></div>
-                              </div>
-                              <div></div>
-                            </div>
-                          </li>
-                          <li>
-                            <div className="d-flex align-items-center">
-                              <div>
-                                <div className="icon icon-xs"></div>
-                              </div>
-                              <div>
-                                <span className="pl-2"></span>
-                              </div>
-                            </div>
-                          </li>
-                        </ul>
                         <button type="button" className="btn btn-primary mb-3">
-                          Start free trial
+                          Register Now
                         </button>
                       </div>
                     </div>
-                    <div className="card card-pricing bg-gradient-success zoom-in shadow-lg rounded border-0 text-center mb-4">
-                      <div className="card-header bg-transparent">
-                        <h4 className="text-uppercase ls-1 text-white py-3 mb-0"></h4>
-                      </div>
-                      <div className="card-body px-lg-7">
-                        <div className="display-1 text-white"></div>
-                        <span className="text-white"></span>
-                        <ul className="list-unstyled my-4">
-                          <li>
-                            <div className="d-flex align-items-center">
-                              <div>
-                                <span className="pl-2 text-white"></span>
-                              </div>
-                            </div>
-                          </li>
-                          <li>
-                            <div className="d-flex align-items-center">
-                              <div></div>
-                              <div>
-                                <span className="pl-2 text-white"></span>
-                              </div>
-                            </div>
-                          </li>
-                          <li>
-                            <div className="d-flex align-items-center">
-                              <div>
-                                <span className="pl-2 text-white"></span>
-                              </div>
-                            </div>
-                          </li>
-                        </ul>
-                      </div>
-                      <div className="card-footer bg-transparent"></div>
+                    <div className="card card-pricing  zoom-in rounded border-0 text-center mb-4">
+                      <img
+                        className="card-img"
+                        src={macbookScreen}
+                        alt="Show Screen"
+                      />
                     </div>
                   </div>
                 </div>
@@ -155,19 +80,6 @@ class HomePage extends Component {
           </div>
           <div className="position-relative">
             <section className="section-hero section-shaped my-0">
-              <div className="shape shape-style-1 shape-primary">
-                <span className="span-150"></span>
-                <span className="span-50"></span>
-                <span className="span-50"></span>
-                <span className="span-75"></span>
-                <span className="span-100"></span>
-                <span className="span-75"></span>
-                <span className="span-50"></span>
-                <span className="span-100"></span>
-                <span className="span-50"></span>
-                <span className="span-100"></span>
-              </div>
-
               <div className="container shape-container d-flex align-items-center">
                 <div className="col px-0">
                   <div className="pricing card-group flex-column flex-md-row mb-3">
