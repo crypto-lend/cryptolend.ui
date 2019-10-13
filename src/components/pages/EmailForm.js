8bfbec8da3e45e1b165d50623c5e0a061c11ee84
jashanbhullar <jsbhullar96@gmail.com>
2019-10-13 15:18:24 +0530

    - FIxing bugs

--- src/components/pages/EmailForm.js
+++ src/components/pages/EmailForm.js
@@ -59,55 +59,54 @@ export default function EmailForm(props) {
 
   return (
     <>
-    <Navbar/>
-    <div className="container mt-5 pt-5">
-      <div className="row  justify-content-center text-center">
-        <div className="col-lg-6 col-10">
-          <h4 className="font-weight-light">{text}</h4>
+      <Navbar />
+      <div className="container mt-5 pt-5">
+        <div className="row  justify-content-center text-center">
+          <div className="col-lg-6 col-10">
+            <h4 className="font-weight-light">{text}</h4>
+          </div>
+        </div>
+        <div className="row mt-5 justify-content-center text-center">
+          <div
+            className="col-lg-6 col-12 py-5 px-5 border"
+            style={{
+              borderRadius: "1rem"
+            }}
+          >
+            <form onSubmit={() => redirect && push(redirect, { email })}>
+              <div className="form-group">
+                <label
+                  className="h5 font-weight-normal"
+                  htmlFor="exampleFormControlInput1"
+                >
+                  Enter your Email
+                </label>
+                <input
+                  type="email"
+                  className="form-control mt-3"
+                  placeholder="your@email.com"
+                  value={email}
+                  onChange={e => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="text-center">
+                <button
+                  type="submit"
+                  className={`btn btn-primary mt-3 w-50 ${
+                    validateEmail(email) ? "" : "disabled"
+                  }`}
+                >
+                  <span className="btn-inner--text">Register Now</span>
+                  <span className="btn-inner--icon ml-3">
+                    <i className="ni ni-bold-right"></i>
+                  </span>
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-      <div className="row mt-5 justify-content-center text-center">
-        <div
-          className="col-lg-6 col-12 py-5 px-5 border"
-          style={{
-            borderRadius: "1rem"
-          }}
-        >
-          <form onSubmit={() => redirect && push(redirect, {email})}>
-            <div className="form-group">
-              <label
-                className="h5 font-weight-normal"
-                htmlFor="exampleFormControlInput1"
-              >
-                Enter your Email
-              </label>
-              <input
-                type="email"
-                className="form-control mt-3"
-                placeholder="your@email.com"
-                value={email}
-                onChange={e => setEmail(e.target.value)}
-                required
-              />
-            </div>
-            <div className="text-center">
-              <button
-                type="button"
-                className={`btn btn-primary mt-3 w-50 ${
-                  validateEmail(email) ? "" : "disabled"
-                }`}
-                onClick={() => redirect && push(redirect)}
-              >
-                <span className="btn-inner--text">Register Now</span>
-                <span className="btn-inner--icon ml-3">
-                  <i className="ni ni-bold-right"></i>
-                </span>
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
     </>
   );
 }
